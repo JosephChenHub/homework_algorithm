e5d8dbd95f14d646eedbfbe94326515db20a6f6b
JosephChen <zychen_1994@163.com>
2017-12-09 23:05:00 +0800

    bug...

--- python/dual_simplex.py
+++ python/dual_simplex.py
@@ -5,51 +5,70 @@ import cvxpy as cvx
 import numpy as np
 
 
-#dual simplex 
-l = 6
-M = np.array([[7,-7,2,1,6,0,-1,0],
-	[3,-1,1,-2,0,0,0,-3],
-	[2,1, 0, 1,1,0,0,4],
-	[-1,3,0,-3,0,1,0,12]])
+#dual simplex
+#construct dual problem
 
-'''
-l = 2
-M = np.array([[13,23, 0,0,0, -1,0],
-			 [ 5,15, 1, 0,0, 0,480],
-			 [4, 4, 0, 1, 0, 0,160],
-			 [35, 20, 0, 0, 1,0, 1190]])
-'''
+
+
+l = 6
+M = np.array([
+	[7,-7,2, 1,6,0, 0,0,0,0,0,0, -1,0],
+	[-3,1,-1,2,0,0,-1,0,0,0,0,0,  0,3],
+	[2, 1,0, 1,1,0,0,-1,0,0,0,0,  0,4],
+	[-1,3,0,-3,0,1,0,0,-1,0,0,0,  0,12],
+	[-3,1,-1,2,0,0,0,0,0,1,0,0,  0,3],
+	[2, 1,0, 1,1,0,0,0,0,0,1,0,  0,4],
+	[-1,3,0,-3,0,1,0,0,0,0,0,1,  0,12],
+])
+
 
 M = M.astype(float)
 rows,cols = M.shape
-times = 0
-while(1):
-	times += 1
+inf = 99999999
+opt_status = True
+last_row = []
+
+for times in range(100):
+	#check whether to stop
 	cnt = 0
 	for i in range(l):
 		if M[0][i] <= 0:
 			cnt += 1
-	if cnt == l: #stop
+	if cnt == l: 
 		break
-	temp = M[0][0:l]
+	#first row 
+	temp = M[0][:l]
 	index_col = temp.argmax()
-	min_val = 0
+	#min ratio rule
+	#if fails, set 0
+	min_val = inf
 	index_row = 1
 	for j in range(1,rows):
-		if abs(M[j][index_col]) > 1e-6: 
-			if min_val > M[j][cols-1]/M[j][index_col]:
-				min_val = M[j][cols-1]/M[j][index_col]
-				index_row = j			
-	for i in range(rows):
-		if not i == index_row:
-			M[i] += -M[i][index_col]* M[index_row]/M[index_row][index_col]			
-		else:
-			M[i] = M[i]/M[i][index_col]
-	if times > 100:	
+		if abs(M[j][index_col]) > 1e-6: #!= 0
+			if min_val > M[j][cols-1]/M[j][index_col] and M[j][cols-1]/M[j][index_col] > 0:
+				if j not in last_row:
+					min_val = M[j][cols-1]/M[j][index_col]
+					index_row = j	
+					last_row.append(j)
+
+	if min_val == inf:
+		print 'unbounded!', M 
+		print 'not fisible!'
+		opt_status = False
 		break
 
-print 'optimal value:', M[0][cols-1]
-print M
+	for i in range(rows):
+		if not(i == index_row):
+			M[i] += -M[i][index_col]* M[index_row]/M[index_row][index_col]			
+		else:
+			M[i] = M[i]/M[index_row][index_col]
+
+	print 'choose :{} {}'.format(index_row, index_col)
+	print 'M:', M
+
+if opt_status == True:
+	print 'optimal value:', M[0][cols-1]
+	print M
 
 
 
@@ -58,12 +77,19 @@ print M
 #using tool --cvxpy
 #construct the problem
 x = cvx.Variable(6)
-a = [-7,7,-2,-1,-6,0]
-objective = cvx.Minimize(a*x)
-constraints = [ [3,-1,1,-2,0,0]*x == -3, 
-	[2,1,0,1,1,0]*x == 4,
-	[-1,3,0,-3,0,1]*x == 12,
-	x >= 0]
+c = np.array([-7,7,-2,-1,-6,0])
+A = np.array([[3,-1,1,-2,0,0],
+	 [2,1,0,1,1,0],
+	 [-1,3,0,-3,0,1]])
+b = np.array([-3,4,12])
+obj = c*x
+objective = cvx.Minimize(obj)
+
+constraints = [ 
+	A*x == b,
+	x >= 0
+	]
+#solve
 prob = cvx.Problem(objective, constraints)
 
 #optimal
